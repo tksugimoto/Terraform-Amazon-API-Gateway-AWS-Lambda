eb74bbcbc679baab2b64fd65fbf5c85d7ff0aba1
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-02-02 21:37:10 +0900

    feat: リクエストのproxy 機能を追加
    ?type=proxy&target=<encodedURL>

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Terraform を使って API Gateway と Lambda を構築する
 ## 機能
 - Lambdaに届いた Event をそのままレスポンスする
 	- `?type=echo` (デフォルト)
+- 別URLへのリクエストをLambdaにプロキシさせる
+	- 制約
+		- GETリクエストのみ
+		- レスポンスはテキストのみ
+	- `?type=proxy&target=<encodedURL>`
+		- `type=proxy`
+		- `target=<encodedURL>`
+			- `target=http%3A%2F%2Fcheckip.amazonaws.com%2F`
+				- `http://checkip.amazonaws.com/`
+			- `target=https%3A%2F%2Fexample.com%2F%3Fa%3D1%26b%3D2`
+				- `https://example.com/?a=1&b=2`
 
 ## 使い方
 1. 事前準備
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+const url = require('url');
+const http = require('http');
+const https = require('https');
+
+const Protocols = {
+	HTTP:  'http:',
+	HTTPS: 'https:',
+};
+
 const EventType = {
 	Echo: 'echo',
+	Proxy: 'proxy',
 };
 
 exports.handler = (event, context, callback) => {
@@ -13,6 +23,67 @@ exports.handler = (event, context, callback) => {
 			body: JSON.stringify(event, null, '\t'),
 		};
 		callback(null, response);
+		return;
+	}
+	if (type === EventType.Proxy) {
+		const target = event.queryStringParameters.target;
+		console.info(`Request proxy started: ${target}`);
+
+		/**
+		 * @param {function} resolve
+		 * @returns {function(http.IncomingMessage): void}
+		 */
+		const onResponse = (resolve) => (res) => {
+			let rawData = '';
+			res.setEncoding('utf8').on('data', (chunk) => {
+				rawData += chunk;
+			}).on('end', () => {
+				resolve(rawData);
+			});
+		};
+
+		Promise.resolve().then(() => {
+			if (!target) throw new Error('target URL is empty.');
+			return new url.URL(target);
+		}).then(targetURL => {
+			if (!Object.values(Protocols).includes(targetURL.protocol)) {
+				throw new Error(`target URL(${targetURL})'s protocol(${targetURL.protocol}) is invalid.`);
+			}
+
+			if (targetURL.protocol === Protocols.HTTP) {
+				return new Promise((resolve, reject) => {
+					http.request(targetURL, onResponse(resolve)).on('error', reject).end();
+				});
+			}
+			if (targetURL.protocol === Protocols.HTTPS) {
+				return new Promise((resolve, reject) => {
+					https.request(targetURL, onResponse(resolve)).on('error', reject).end();
+				});
+			}
+		}).then(responseBody => {
+			console.info(`Request proxy completed: ${target}`);
+			console.info({
+				responseBody,
+			});
+			const response = {
+				statusCode: 200,
+				headers: {
+					'Content-Type': 'text/plain',
+				},
+				body: responseBody,
+			};
+			callback(null, response);
+		}).catch(err => {
+			console.error(err);
+			const response = {
+				statusCode: 400,
+				headers: {
+					'Content-Type': 'text/plain',
+				},
+				body: err.message,
+			};
+			callback(null, response);
+		});
 		return;
 	}
 	const errMessage = `type(${type}) is invalid.`;
